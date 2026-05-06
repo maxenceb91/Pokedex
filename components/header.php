31c409ea859256200b718aae356947d637d6a62b
Maxence <169144059+maxenceb91@users.noreply.github.com>
2026-05-06 13:19:53 +0200

    refactor: header

--- components/header.php
+++ components/header.php
@@ -1,18 +1,21 @@
 <header class="flex items-center justify-between p-15 bg-white shadow-md mb-10">
-    <h1 class="text-2xl font-bold text-gray-800">Pokédex</h1>
+    <div class="flex items-center gap-4">
+        <img src="/Pokedex/assets/pokeball.png" alt="Pokéball" class="w-10 h-10">
+        <h1 class="text-2xl font-bold text-gray-800">Pokédex</h1>
+    </div>
     <nav>
-        <ul class="flex items-center gap-6">
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
-                <a href="../index.php">Accueil</a>
+        <ul class="flex items-center gap-10">
+            <li class="group text-lg font-medium text-gray-700">
+                <a href="../index.php" class="relative inline-block pb-1 transition-colors duration-300 ease-in-out hover:text-red-600 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:origin-left after:scale-x-0 after:bg-red-600 after:transition-transform after:duration-300 after:ease-in-out group-hover:after:scale-x-100">Accueil</a>
             </li>
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
-                <a href="types.php">Types</a>
+            <li class="group text-lg font-medium text-gray-700">
+                <a href="types.php" class="relative inline-block pb-1 transition-colors duration-300 ease-in-out hover:text-red-600 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:origin-left after:scale-x-0 after:bg-red-600 after:transition-transform after:duration-300 after:ease-in-out group-hover:after:scale-x-100">Types</a>
             </li>
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
-                <a href="team.php">Équipe</a>
+            <li class="group text-lg font-medium text-gray-700">
+                <a href="signup.php" class="relative inline-block pb-1 transition-colors duration-300 ease-in-out hover:text-red-600 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:origin-left after:scale-x-0 after:bg-red-600 after:transition-transform after:duration-300 after:ease-in-out group-hover:after:scale-x-100">Inscription</a>
             </li>
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
-                <a href="profile.php">Profil</a>
+            <li class="group text-lg font-medium text-gray-700">
+                <a href="login.php" class="relative inline-block pb-1 transition-colors duration-300 ease-in-out hover:text-red-600 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full after:origin-left after:scale-x-0 after:bg-red-600 after:transition-transform after:duration-300 after:ease-in-out group-hover:after:scale-x-100">Connexion</a>
             </li>
         </ul>
     </nav>
