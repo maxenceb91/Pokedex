9a7c2b8161593db7324dec1f88725634ef6f4d00
Maxence <169144059+maxenceb91@users.noreply.github.com>
2026-05-06 11:18:49 +0200

    Update header.php

--- components/header.php
+++ components/header.php
@@ -2,10 +2,18 @@
     <h1 class="text-2xl font-bold text-gray-800">Pokédex</h1>
     <nav>
         <ul class="flex items-center gap-6">
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200"><a href="#">Accueil</a></li>
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200"><a href="#">Types</a></li>
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200"><a href="#">Équipe</a></li>
-            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200"><a href="#">Profil</a></li>
+            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
+                <a href="../index.php">Accueil</a>
+            </li>
+            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
+                <a href="types.php">Types</a>
+            </li>
+            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
+                <a href="team.php">Équipe</a>
+            </li>
+            <li class="text-lg font-medium text-gray-700 hover:text-blue-500 hover:cursor-pointer hover:transition-colors duration-200">
+                <a href="profile.php">Profil</a>
+            </li>
         </ul>
     </nav>
 </header>
