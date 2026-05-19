4f55708b33b8069551e7889756bf0cc6c4a27d46
Maxence <169144059+maxenceb91@users.noreply.github.com>
2026-05-19 09:35:21 +0200

    fix: add missing favicon link in head.php

--- components/head.php
+++ components/head.php
@@ -8,6 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokédex</title>
+    <link rel="icon" type="image/png" href="assets/pokeball.png">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
